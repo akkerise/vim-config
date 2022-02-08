7d8f9b3df0ba6a2a5efb3c9d6eac35f2328607d4
thanhnaxlts <thanhna@lqa.com.vn>
2022-02-08 19:12:04 +0700

    update move/init.sh

--- move/init.sh
+++ move/init.sh
@@ -6,10 +6,21 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 #alacritty
 cd ~
 mkdir alacritty
-mv ~/.config/nvim/move/alacritty.yml ~/.config/alacritty
+cp ~/.config/nvim/move/alacritty.yml ~/alacritty
 
 #tmux
-mv ~/.config/nvim/move/.tmux.conf ~/
-
+cp ~/.config/nvim/move/.tmux.conf ~/
 
 echo "alias vim=\"nvim\"" >> ~/.zshrc
+
+#yay
+git clone https://aur.archlinux.org/yay-git.git ~/yay
+cd ~/yay
+makepkg -si
+
+#google-chrome
+yay -S google-chrome
+yay -Syu
+
+
+
